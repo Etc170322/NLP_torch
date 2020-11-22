10eb05ce87cc4ca99a49e6f2ea41b18f881cf10e
Etc170322 <chencheng1988621@163.com>
2020-11-22 18:25:32 +0800

    TextCNN
    Pytorch版CNN初稿

--- core/TextCNN.py
+++ core/TextCNN.py
@@ -1,15 +1,12 @@
 import torch
 from torch import nn
-import torch.functional as F
+import torch.nn.functional as F
 import numpy as np
 from utils import *
 
 class Model(nn.Module):
     def __init__(self,config):
         self.config = read_config(config)
-        self.train_path = self.config["train_path"]
-        self.test_path = self.config["test_path"]
-        self.dev_path = self.config["dev_path"]
         self.class_list = self.config["class_list"]
         self.vocab_path = self.config["vocab_path"]
         self.model_path = self.config["model_path"]
@@ -17,17 +14,33 @@ class Model(nn.Module):
         self.filter_num = self.config["filter_num"]
 
         self.require_improvement = self.config["require_improvement"]
-        self.n_vocab = 0
+        self.n_vocab = self.config['vocabulary_size']
+        self.embedding_dim = self.config['Embedding_dim']
         self.num_epochs = self.config["num_epochs"]
         self.batch_size = self.config["batch_size"]
         self.pad_size = self.config["pad_size"]
-        self.dropout = self.config["dropout"]
+        self.dropout = nn.Dropout(self.config["dropout"])
         self.device = torch.device('cuda')
         self.Embedding_pretrained = self.config["Embedding_pretrained"]
 
+        self.convs = nn.ModuleList([nn.Conv2d(1,self.filter_num, self.embedding_dim, filter_size) for filter_size in self.filter_sizes])
+        self.fc = nn.Linear(self.filter_num*len(self.filter_sizes),len(self.class_list))
 
     def embedding(self):
         if self.Embedding_pretrained is not None:
             self.Embedding = nn.Embedding.from_pretrained(self.config["Pretrain_Embedding_path"],freeze=False)
         else:
-            self.Embedding = nn.Embedding(self.)
+            self.Embedding = nn.Embedding(self.n_vocab,self.embedding_dim,padding_idx=self.n_vocab-1)
+
+    def conv_and_pool(self,x, conv):
+        x = F.relu(conv(x)).sequeeze(3)
+        x = F.max_pool1d(x,x.size(2)).sequeeze(2)
+        return x
+
+    def forward(self, x):
+        out = self.Embedding(x[0])
+        out = out.unsequeeze(1)
+        out = torch.cat([self.conv_and_pool(out,conv) for conv in self.convs],1)
+        out = self.dropout(out)
+        out = self.fc(out)
+        return out
--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
 import os
 import json
+import torch
 from tqdm import tqdm
 from collections import defaultdict
 import pickle as pkl
+import time
+from datetime import timedelta
+import jieba
 
-def build_vocab(file_path, tokenizer, max_size, min_freq):
+def build_vocab(file_path, use_words, max_size, min_freq):
     vocab_dic = defaultdict(int)
     with open(file_path, 'r', encoding='UTF-8') as f:
         for line in tqdm(f):
@@ -12,7 +16,12 @@ def build_vocab(file_path, tokenizer, max_size, min_freq):
             if not lin:
                 continue
             content = lin.split('\t')[0]
-            for word in tokenizer(content):
+            if use_words:
+                tokens = list(jieba.cut(content))
+            else:
+                tokenizer = lambda x: [y for y in x]
+                tokens = tokenizer(content)
+            for word in tokens:
                 vocab_dic[word] = vocab_dic[word] + 1
         vocab_list = sorted([_ for _ in vocab_dic.items() if _[1] >= min_freq], key=lambda x: x[1], reverse=True)[:max_size]
         vocab_dic = {word_count[0]: idx for idx, word_count in enumerate(vocab_list)}
@@ -24,16 +33,18 @@ def read_config(path):
         config = json.load(f)
     return config
 
-def build_dataset(config, ues_word):
-    if ues_word:
-        tokenizer = lambda x: x.split(' ')  # 以空格隔开，word-level
+def get_time_dif(start_time):
+    """获取已使用时间"""
+    end_time = time.time()
+    time_dif = end_time - start_time
+    return timedelta(seconds=int(round(time_dif)))
+
+def build_dataset(config, use_words):
+    if os.path.exists(config.get('data','vocab_path')):
+        vocab = pkl.load(open(config.get('data','vocab_path'), 'rb'))
     else:
-        tokenizer = lambda x: [y for y in x]  # char-level
-    if os.path.exists(config.vocab_path):
-        vocab = pkl.load(open(config.vocab_path, 'rb'))
-    else:
-        vocab = build_vocab(config.train_path, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
-        pkl.dump(vocab, open(config.vocab_path, 'wb'))
+        vocab = build_vocab(config.get('data','train_path'), use_words=use_words, max_size=int(config.get('model','MAX_VOCAB')), min_freq=1)
+        pkl.dump(vocab, open(config.get('data','vocab_path'), 'wb'))
     print(f"Vocab size: {len(vocab)}")
 
     def load_dataset(path, pad_size=32):
@@ -45,20 +56,72 @@ def build_dataset(config, ues_word):
                     continue
                 content, label = lin.split('\t')
                 words_line = []
-                token = tokenizer(content)
-                seq_len = len(token)
+                if use_words:
+                    tokens = list(jieba.cut(content))
+                else:
+                    tokenizer = lambda x: [y for y in x]
+                    tokens = tokenizer(content)
+                seq_len = len(tokens)
                 if pad_size:
-                    if len(token) < pad_size:
-                        token.extend(['PAD'] * (pad_size - len(token)))
+                    if len(tokens) < pad_size:
+                        tokens.extend(['PAD'] * (pad_size - len(tokens)))
                     else:
-                        token = token[:pad_size]
+                        tokens = tokens[:pad_size]
                         seq_len = pad_size
                 # word to id
-                for word in token:
-                    words_line.append(vocab.get(word, vocab.get(UNK)))
+                for word in tokens:
+                    words_line.append(vocab.get(word, vocab.get('UNK')))
                 contents.append((words_line, int(label), seq_len))
         return contents  # [([...], 0), ([...], 1), ...]
-    train = load_dataset(config.train_path, config.pad_size)
-    dev = load_dataset(config.dev_path, config.pad_size)
-    test = load_dataset(config.test_path, config.pad_size)
+    train = load_dataset(config.get('data','train_path'), int(config.get('model','max_length')))
+    dev = load_dataset(config.get('data','dev_path'), int(config.get('model','max_length')))
+    test = load_dataset(config.get('data','test_path'), int(config.get('model','max_length')))
     return vocab, train, dev, test
+
+class DatasetIterater(object):
+    def __init__(self, batches, batch_size, device):
+        self.batch_size = batch_size
+        self.batches = batches
+        self.n_batches = len(batches) // batch_size
+        self.residue = False  # 记录batch数量是否为整数
+        if len(batches) % self.n_batches != 0:
+            self.residue = True
+        self.index = 0
+        self.device = torch.device(device)
+
+    def _to_tensor(self, datas):
+        x = torch.LongTensor([_[0] for _ in datas]).to(self.device)
+        y = torch.LongTensor([_[1] for _ in datas]).to(self.device)
+
+        # pad前的长度(超过pad_size的设为pad_size)
+        seq_len = torch.LongTensor([_[2] for _ in datas]).to(self.device)
+        return (x, seq_len), y
+
+    def __next__(self):
+        if self.residue and self.index == self.n_batches:
+            batches = self.batches[self.index * self.batch_size: len(self.batches)]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+        elif self.index >= self.n_batches:
+            self.index = 0
+            raise StopIteration
+        else:
+            batches = self.batches[self.index * self.batch_size: (self.index + 1) * self.batch_size]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        if self.residue:
+            return self.n_batches + 1
+        else:
+            return self.n_batches
+
+def build_iterator(dataset, config):
+    iter = DatasetIterater(dataset, int(config.get('model','batch_size')), config.get('model','device'))
+    return iter
